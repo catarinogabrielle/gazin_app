2e7c2063bee6cb93420b04e7af05aea346a01be4
Gabrielle <gabrielle.castro@ymonetize.com>
2023-05-01 01:26:37 -0300

    atualização

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { ActivityIndicator, Platform } from 'react-native';
 import YoutubePlayer from "react-native-youtube-iframe";
 
@@ -7,6 +7,8 @@ import { Container, Header, Logo, TextLogo, Content, BoxVideo, ContentInfo, Text
 import Colors from '../../../constants/Colors';
 const ColorTheme = Colors['Theme'];
 
+import api from '../../../contexts/api.json'
+
 var shadow = {
     elevation: 3,
     shadowColor: "grey",
@@ -14,9 +16,14 @@ var shadow = {
     shadowRadius: 10,
 }
 
-console.log(JSON.stringify(Platform.constants.Model, null, 2))
-
 export default function Home() {
+    const [phone, setPhone] = useState('')
+
+    useEffect(() => {
+        const platform = JSON.stringify(Platform.constants.Model, null, 2)
+        setPhone(platform)
+    }, [])
+
     return (
         <Container>
             <Header>
@@ -30,17 +37,21 @@ export default function Home() {
                     <YoutubePlayer
                         width="100%"
                         height={222}
-                        videoId={"IentQeNVJFU"}
+                        videoId={api.link}
                         play={true}
                     />
                 </BoxVideo>
 
-                <ContentInfo style={shadow}>
-                    <Text>Celular Samsung galaxy a-53 5g 128gb dual</Text>
-                    <Label>Preço à Vista: <Text style={{ color: ColorTheme.Azul }}>R$ 2.299,00</Text></Label>
-                    <Label>Preço à Prazo: <Text style={{ color: ColorTheme.Azul }}>R$ 2.299,00</Text></Label>
-                    <LabelInfo>em <Text style={{ color: ColorTheme.Laranja }}>12x</Text> de <Text style={{ color: ColorTheme.Laranja }}>R$ 64,42 </Text>sem juros</LabelInfo>
-                </ContentInfo>
+                {api.rows.map(item => {
+                    if (`"${item.code}"` == phone) return (
+                        <ContentInfo key={item.id} style={shadow}>
+                            <Text>{item.name}</Text>
+                            <Label>Preço à Vista: <Text style={{ color: ColorTheme.Azul }}>R$ {item.cash_price}</Text></Label>
+                            <Label>Preço à Prazo: <Text style={{ color: ColorTheme.Azul }}>R$ {item.term_price}</Text></Label>
+                            <LabelInfo>em <Text style={{ color: ColorTheme.Laranja }}>{item.parcel}x</Text> de <Text style={{ color: ColorTheme.Laranja }}>R$ {item.value_parcel} </Text>sem juros</LabelInfo>
+                        </ContentInfo>
+                    )
+                })}
             </Content>
         </Container>
     )
