436024de5f5ecd8a0a4f623b374b42e6678135cb
Gabrielle <gabrielle.castro@ymonetize.com>
2023-05-19 10:02:58 -0300

    atualização

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -176,7 +176,7 @@ export default function Home() {
                             <YoutubePlayer
                                 width="100%"
                                 height={222}
-                                videoId={"BrX5P0iJsLM"}
+                                videoId={"mO7JGYLjXZ0"}
                                 play={true}
                             />
                         </BoxVideo>
