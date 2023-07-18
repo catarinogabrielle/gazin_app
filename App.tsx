a23edf18eda49bf677f989f21766843dda2317e1
Gabrielle <gabrielle.castro@ymonetize.com>
2023-07-18 15:19:00 -0300

    atualização

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { View, StatusBar } from 'react-native';
+import { useKeepAwake } from 'expo-keep-awake';
 
 import Home from './src/pages/Home'
 
 export default function App() {
+  useKeepAwake()
   return (
     <View>
       <StatusBar backgroundColor="#312783" translucent={false} />
