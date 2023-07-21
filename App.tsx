4bc0aea7292a29cfaf4da2485509e8a53b80f0ef
Gabrielle <gabrielle.castro@ymonetize.com>
2023-07-21 03:06:11 -0300

    atualização

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import { useKeepAwake } from 'expo-keep-awake';
 import Home from './src/pages/Home'
 
 export default function App() {
+  useKeepAwake()
   return (
     <View>
       <StatusBar backgroundColor="#312783" translucent={false} />
